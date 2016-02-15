bd640ab64b190821d70a7cdbb2b8b2861a990f95
Petr Gazarov <petr@policygenius.com>
2016-02-15 10:56:05 -0500

    Customize for team use

--- app/models/engineer_notifier.rb
+++ app/models/engineer_notifier.rb
@@ -2,25 +2,27 @@ require 'slack-ruby-bot'
 require 'json'
 
 class EngineerNotifier < SlackRubyBot::Bot
-  
   MENTIONS = JSON.parse ENV['MENTIONS']
- 
-  command 'ping' do |client, data, match|
-    client.say(text: 'pong', channel: data.channel)
-  end
+  CHANNELS = JSON.parse ENV['CHANNELS']
 
   match /.*/ do |client, data, match|
-    data[:attachments].each do |attachment|
-      MENTIONS.each do |mention|
-        if attachment.text.include?(mention)
-          echo_mention(data, client, mention)
+    unless valid_channel?(data.channel)
+      data[:attachments].try(:each) do |attachment|
+        MENTIONS.each do |mention|
+          if attachment.text.include?(mention)
+            echo_mention(data: data, client: client, mention: mention)
+          end
         end
       end
     end
   end
 
-  def self.echo_mention(data, client, mention)
+  def self.echo_mention(data:, client:, mention:)
     client.say(channel: data.channel, text: "^ #{mention}")
+  end
+
+  def self.valid_channel?(channel)
+    CHANNELS.include?(channel)
   end
 end
 
