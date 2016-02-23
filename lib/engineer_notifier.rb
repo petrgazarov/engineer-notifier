d1f23270946ef8fff50b0489ad1e550f0d01f586
Petr Gazarov <petrgazarov@gmail.com>
2016-02-23 01:08:37 -0500

    Improve user mentions

--- lib/engineer_notifier.rb
+++ lib/engineer_notifier.rb
@@ -3,30 +3,29 @@ require 'json'
 
 module SlackBot
   class EngineerNotifier < SlackRubyBot::Bot
-    MENTIONS = JSON.parse ENV['MENTIONS']
+
+    MENTIONS_TO_HANDLES = JSON.parse ENV['MENTIONS_TO_HANDLES']
     CHANNEL_IDS = JSON.parse ENV['CHANNEL_IDS']
     BOT_EMOJI = ENV['BOT_EMOJI'] || ':dragon_face:'
 
     match /.*/ do |client, data, match|
-      if post_includes_bot_emoji?(data)
-        post_emoji_reaction(data: data, client: client)
-      end
+      post_emoji_reaction(data: data, client: client) if post_includes_bot_emoji?(data)
 
       if valid_channel?(data.channel)
         data[:attachments].try(:each) do |attachment|
           next unless attachment.text
 
-          MENTIONS.each do |mention|
+          MENTIONS_TO_HANDLES.each do |mention, handle|
             if attachment.text.include?(mention)
-              echo_mention(data: data, mention: mention, client: client)
+              post_notification(data: data, handle: handle, client: client)
             end
           end
         end
       end
     end
 
-    def self.echo_mention(data:, mention:, client:)
-      client.say(channel: data.channel, text: "^ #{mention}")
+    def self.post_notification(data:, handle:, client:)
+      client.say(channel: data.channel, text: "^ <#{handle}>")
     end
 
     def self.valid_channel?(channel_id)
